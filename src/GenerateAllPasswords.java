5e4be579da1ed99aa111152b993492ded0e5ca7c
Mohit Gupta <mohit.gupta2jan@gmail.com>
2021-05-11 21:47:08 +0530

    Update GenerateAllPasswords.java

--- src/GenerateAllPasswords.java
+++ src/GenerateAllPasswords.java
@@ -25,3 +25,55 @@
         yy
 */
 
+import java.util.Scanner;
+
+class Source 
+{
+    // int cnt;
+    // Recursive helper function, adds/removes characters until len is reached
+    static void generate(char[] arr, int i, String s, int len)
+    {
+        // base case
+        if (i == 0) // when len has been reached
+        {
+            // print it out
+            System.out.println(s);
+             
+            // cnt++;
+            return;
+        }
+ 
+        // iterate through the array
+        for (int j = 0; j < len; j++)
+        {
+            // Create new string with next character Call generate again until string has reached its len
+            String appended = s + arr[j];
+            generate(arr, i - 1, appended, len);
+        }
+        return;
+    }
+ 
+    // function to generate all possible passwords
+    static void genPW(char[] arr, int len)
+    {
+        // call for all required lengths
+        for (int i = 1; i <= len; i++)
+        {
+            generate(arr, i, "", len);
+        }
+    } 
+  
+    public static void main(String[] args) 
+    { 
+        int n;
+        Scanner s = new Scanner(System.in);
+        n = s.nextInt();
+        char arr[] = new char[n];
+        
+        for(int i = 0; i < n; i++) {
+            arr[i] = s.next().charAt(0);
+        }
+        
+        genPW(arr, n);
+    }
+}
