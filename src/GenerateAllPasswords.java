ff089ac08c9f38258a7d515bb6f7cb39a6a1d741
Mohit Gupta <mohit.gupta2jan@gmail.com>
2021-05-11 21:45:57 +0530

    Create GenerateAllPasswords.java

---
+++ src/GenerateAllPasswords.java
@@ -0,0 +1,27 @@
+/*
+  Generate All Passwords
+    Description
+      You are given a set of characters. You need to generate all possible passwords from them. 
+      For this, you need to find all possible permutations of the given characters.
+
+    Input Format:
+      The input contains the number of characters, followed by the characters.
+
+
+    Output Format:
+      The output contains all the possible passwords.
+
+
+    Sample Test Cases:
+      Input:
+        2 x y
+
+      Output:
+        x
+        y
+        xx
+        xy
+        yx
+        yy
+*/
+
